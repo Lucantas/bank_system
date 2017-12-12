5694ed3877d31d02846c6da4a498d3e11bcd16d5
Lucas D <lucas.lucantas38@gmail.com>
2017-12-12 17:51:44 -0200

    testing new class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #This is a ATM project, simulating the interations with a bank and a client, to create your bank account give us a call : 0800 not
-from conta import Conta
+from Conta import Conta
+from Conta_Poup import Conta_Poup
 
 contas = []
 
@@ -10,7 +11,7 @@ def cria_conta(Conta, contas):
     agrupa_contas(Conta, contas)
 
 cria_conta(Conta(1,1000,"Lucas",5000), contas)
-cria_conta(Conta(2,2000, "Monique",5000), contas)
+cria_conta(Conta_Poup(2,2000, "Monique",5000), contas)
 
 contas[0].deposita(600)
 contas[0].transfere(500,contas[1])
@@ -20,6 +21,7 @@ contas[0].limite = 10000
 #print("O saldo da conta do(a) {} é {} ".format(contas[1].titular, contas[1].saldo))
 #print("O saldo da conta do(a) {} é {} ".format(contas[0].titular, contas[0].saldo))
 
-print("O limite da conta do {} é: {} ".format(contas[0].titular, contas[0].limite))
+print("O saldo da conta da {} é {}".format(contas[1].titular, contas[1].saldo))
+print("O limite da conta do {} é: {} no banco com o código {}.".format(contas[0].titular, contas[0].limite, contas[0].codigo_banco()))
 
 
